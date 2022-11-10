41ce08ed0293a58598f52449ee933ab821a5c3f8
eobcre <eobcre@gmail.com>
2022-11-09 21:52:46 -0500

    Added API

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 const Form = () => {
+  const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch(
+      "https://api.weatherapi.com/v1/current.json?key=e83949a5a85a460e8e923412220811&q=London&aqi=no"
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <form>
-      <input type="text" name="city" placeholder="Search City" />
-      <button type="submit">
+      <input
+        type="text"
+        name="city"
+        placeholder="Search City"
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button type="submit" onClick={getWeather}>
         <FaSearch />
       </button>
     </form>
