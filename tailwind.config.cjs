891b4118db8294fa94813ce9b8b2857408b4a225
eobcre <eobcre@gmail.com>
2022-11-14 08:36:36 -0500

    Styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        glass: 'rgba(255, 255, 255, 0.14)',
+      },
+      boxShadow: {
+        xl: '0 4px 30px rgba(0, 0, 0, 0.1)',
+      },
       fontFamily: {
         Montserrat: ['Montserrat'],
       },
