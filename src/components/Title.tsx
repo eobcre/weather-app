5e81a659b8f595836c1af08d37429c28f0a69e5d
eobcre <eobcre@gmail.com>
2022-12-06 19:35:59 -0500

    Text size changed

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,7 +2,9 @@ const Title = () => {
   return (
     // JSX
     <div>
-      <p className='text-5xl text-white font-Montserrat py-6'>Weather App</p>
+      <p className='text-3xl text-white font-Montserrat py-6 md:text-5xl'>
+        Weather App
+      </p>
     </div>
   );
 };
