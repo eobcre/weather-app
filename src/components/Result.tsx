8780bd575439185e48e119d772b869e85d8bb6f0
eobcre <eobcre@gmail.com>
2023-03-01 12:40:23 -0500

    variables changed

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,6 +1,6 @@
-// Type declaration
-type ResultsPropsType = {
-  results: {
+// Type
+type ResultPropsType = {
+  result: {
     country: string;
     cityName: string;
     temperature: string;
@@ -9,24 +9,24 @@ type ResultsPropsType = {
   };
 };
 
-export const Result = (props: ResultsPropsType) => {
+export const Result = (props: ResultPropsType) => {
   return (
     // JSX
     <div className='text-xl text-white'>
-      {props.results.country && <div>{props.results.country}</div>}
-      {props.results.cityName && (
-        <div className='py-4'>{props.results.cityName}</div>
+      {props.result.country && <div>{props.result.country}</div>}
+      {props.result.cityName && (
+        <div className='py-4'>{props.result.cityName}</div>
       )}
-      {props.results.temperature && (
+      {props.result.temperature && (
         <div>
-          {props.results.temperature}
+          {props.result.temperature}
           <span> °F</span>
         </div>
       )}
-      {props.results.conditionText && (
+      {props.result.conditionText && (
         <div>
-          <img className='mx-auto' src={props.results.icon} alt='Icon' />
-          <span>{props.results.conditionText}</span>
+          <img className='mx-auto' src={props.result.icon} alt='Icon' />
+          <span>{props.result.conditionText}</span>
         </div>
       )}
     </div>
